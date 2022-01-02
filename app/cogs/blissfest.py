f9e896af18030cd097cd4a5069b4aecd87c8d2b1
Zee Aslam <zeet6613@gmail.com>
2022-01-02 18:39:23 -0500

    refactor

---
+++ app/cogs/blissfest.py
@@ -0,0 +1,24 @@
+
+import disnake
+from disnake.ext import commands
+import requests
+
+
+class BlissfestCommand(commands.Cog):
+    """This will be for a ping command."""
+
+    def __init__(self, bot: commands.Bot, blissfest_excite_url):
+        self.bot = bot
+        self.excite_url = blissfest_excite_url
+
+    @commands.command()
+    async def blissfest(self, ctx: commands.Context):
+        """Pull an image from inspiroBot allegedly"""
+        blissfest_excite_url = self.excite_url
+        msg = disnake.Embed()
+        msg.set_image(url=blissfest_excite_url)
+        await ctx.send(embed=msg)
+
+def setup(bot: commands.Bot):
+    api_url="https://github.com/h3mmy/BloopyBoi/raw/main/app/assets/blissfest_1.gif"
+    bot.add_cog(BlissfestCommand(bot, api_url))
