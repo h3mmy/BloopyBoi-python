1078b8b3ffa1de0df4da7950ecb4ca9ff5dffbad
Zee Aslam <zeet6613@gmail.com>
2022-02-21 21:14:48 -0500

    Add Greeter

---
+++ app/cogs/greetings_test.py
@@ -0,0 +1,19 @@
+"""Unit Tests For greetings Cog"""
+import unittest
+from disnake.ext import commands
+
+from greetings import Greetings  # pylint: disable=import-error
+
+class GreetingsTest(unittest.TestCase):
+    """Unit Tests"""
+
+    def setUp(self) -> None:
+        test_bot = commands.Bot()
+        self.test_cog = Greetings(bot=test_bot)
+
+    def test_constructs_cog(self):
+        """Unit Tests"""
+        self.assertIsInstance(self.test_cog, commands.Cog)
+
+if __name__ == '__main__':
+    unittest.main()
